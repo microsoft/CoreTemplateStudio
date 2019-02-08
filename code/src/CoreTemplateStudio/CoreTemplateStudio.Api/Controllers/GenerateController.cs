3fb38aff3d0e28fdb291a58c23fc05f53f3f9ab3
Danish Dua <danish@dandua.com>
2019-02-07 19:10:43 -0800

    added scaffolding for generate endpoint (#40)
    * added scaffolding for generate endpoint
    
    * Update GenerateModel.cs

---
+++ code/src/CoreTemplateStudio/CoreTemplateStudio.Api/Controllers/GenerateController.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CoreTemplateStudio.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreTemplateStudio.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerateController : Controller
+    {
+        [HttpPost]
+        public JsonResult Generate([FromBody]GenerateModel generationHelper)
+        {
+            (bool valid, string error) = generationHelper.Validate();
+            if (!valid)
+            {
+                return Json(BadRequest(new { message = error }));
+            }
+
+            return Json(Ok(new { wasUpdated = generationHelper.Generate() }));
+        }
+    }
+}
---
+++ code/src/CoreTemplateStudio/CoreTemplateStudio.Api/Models/GenerateModel.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using CoreTemplateStudio.Api.Enumerables;
+
+namespace CoreTemplateStudio.Api.Models
+{
+    public class GenerateModel
+    {
+        public Platform Platform { get; set; }
+
+        public ProjectType ProjectType { get; set; }
+
+        public Framework? Frontend { get; set; }
+
+        public Framework? Backend { get; set; }
+
+        public List<Page> Pages { get; set; }
+
+        public List<Feature> Features { get; set; }
+
+        public (bool, string) Validate()
+        {
+            if (Frontend != null && (Pages == null || Pages.Count == 0))
+            {
+                return (false, "specify at least one page if frontend framework is specified");
+            }
+
+            return (true, string.Empty);
+        }
+
+        public bool Generate()
+        {
+            return true;
+        }
+    }
+}
