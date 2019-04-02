2a7111d8a9171e39e3a63315bda61528c14acec7
Sahil Tara <t-satara@microsoft.com>
2019-04-01 22:18:43 -0700

    Added Json PostAction

---
+++ code/src/CoreTemplateStudio/CoreTemplateStudio.Core/PostActions/Catalog/AddJsonDictionaryItem/AddJsonDictionaryItemPostAction.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.Templates.Core.Templates;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Templates.Core.PostActions.Catalog.AddJsonDictionaryItem
+{
+    public class AddJsonDictionaryItemPostAction : TemplateDefinedPostAction
+    {
+        public const string Id = "CB387AC0-16D0-4E07-B41A-F1EA616A7CA9";
+
+        private readonly Dictionary<string, string> _parameters;
+
+        private readonly string _destinationPath;
+
+        public override Guid ActionId { get => new Guid(Id); }
+
+        public AddJsonDictionaryItemPostAction(string relatedTemplate, IPostAction templatePostAction, Dictionary<string, string> parameters, string destinationPath)
+            : base(relatedTemplate, templatePostAction)
+        {
+            _parameters = parameters;
+            _destinationPath = destinationPath;
+        }
+
+        internal override void ExecuteInternal()
+        {
+            var parameterReplacements = new FileRenameParameterReplacements(_parameters);
+
+            var jsonPath = Path.Combine(_destinationPath, parameterReplacements.ReplaceInPath(Args["jsonPath"]));
+
+            var keyToDict = Args["key"];
+
+            JObject json = JObject.Parse(File.ReadAllText(jsonPath));
+
+            var dictContent = json[keyToDict].ToObject<Dictionary<string, string>>();
+            var contentToAdd = JsonConvert.DeserializeObject<Dictionary<string, string>>(Args["dict"]);
+
+            var newContent = dictContent.Merge(contentToAdd);
+
+            json[keyToDict] = JObject.FromObject(newContent);
+
+            File.WriteAllText(jsonPath, json.ToString(Formatting.Indented));
+        }
+    }
+}
