098f09130d8fa588660cf631a6762dd17d39500b
Sahil Tara <t-satara@microsoft.com>
2019-04-15 14:41:13 -0700

    Sort project templates

--- code/src/CoreTemplateStudio/CoreTemplateStudio.Core/Gen/GenComposer.cs
+++ code/src/CoreTemplateStudio/CoreTemplateStudio.Core/Gen/GenComposer.cs
@@ -60,7 +60,9 @@ namespace Microsoft.Templates.Core.Gen
 
         private static void AddProject(UserSelection userSelection, List<GenInfo> genQueue)
         {
-            var projectTemplates = GenContext.ToolBox.Repo.GetTemplates(TemplateType.Project, userSelection.Platform, userSelection.ProjectType, userSelection.FrontEndFramework, userSelection.BackEndFramework);
+            var projectTemplates = GenContext.ToolBox.Repo.GetTemplates(TemplateType.Project, userSelection.Platform, userSelection.ProjectType, userSelection.FrontEndFramework, userSelection.BackEndFramework).ToList();
+
+            projectTemplates = projectTemplates.OrderBy(projectTemplate => projectTemplate.Identity).ToList();
 
             foreach (var projectTemplate in projectTemplates)
             {
