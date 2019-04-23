6136f696d4e5f10af6a5dc2142b34bdfc3034829
Sahil Tara <t-satara@microsoft.com>
2019-04-23 09:49:33 -0700

    Added port through command line capabilities (#124)

--- code/src/CoreTemplateStudio/CoreTemplateStudio.Api/Program.cs
+++ code/src/CoreTemplateStudio/CoreTemplateStudio.Api/Program.cs
@@ -2,8 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace CoreTemplateStudio.Api
 {
@@ -14,8 +16,20 @@ namespace CoreTemplateStudio.Api
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(configuration)
+                .UseIISIntegration()
                 .UseStartup<Startup>();
+
+            return host;
+        }
     }
 }
