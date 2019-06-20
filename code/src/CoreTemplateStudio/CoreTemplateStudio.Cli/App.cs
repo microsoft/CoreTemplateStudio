37d1b3bedd420b2fff1182852140a5332fa6d8ce
David Gómez <v-davgom@microsoft.com>
2019-06-20 15:16:17 +0200

    Update regex expression to split command args

--- code/src/CoreTemplateStudio/CoreTemplateStudio.Cli/App.cs
+++ code/src/CoreTemplateStudio/CoreTemplateStudio.Cli/App.cs
@@ -14,7 +14,7 @@ namespace Microsoft.Templates.Cli
     public class App
     {
         private readonly string promptSymbol = ">> ";
-        private readonly string splitPattern = @"""(?:(?<= "")([^""]+)""\s*)|\s*([^""\s]+)";
+        private readonly string splitPattern = "(?<=^[^\"]*(?:\"[^\"]*\"[^\"]*)*) (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
         private readonly ICommandDispatcher _dispatcher;
         private readonly IMessageService _messageService;
 
