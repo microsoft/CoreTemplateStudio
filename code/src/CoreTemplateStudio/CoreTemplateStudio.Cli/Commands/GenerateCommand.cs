f8f64dd7dcacfecb736d702bf6491ffe030513d0
David Gómez <v-davgom@microsoft.com>
2019-06-26 16:00:35 +0200

    Fix stylecop

--- code/src/CoreTemplateStudio/CoreTemplateStudio.Cli/Commands/GenerateCommand.cs
+++ code/src/CoreTemplateStudio/CoreTemplateStudio.Cli/Commands/GenerateCommand.cs
@@ -2,9 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using CommandLine;
 using Microsoft.Templates.Cli.Commands.Contracts;
-using System.Collections.Generic;
 
 namespace Microsoft.Templates.Cli.Commands
 {
