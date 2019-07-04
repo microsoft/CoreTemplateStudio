9a055c7b8d42f8e34829632265a9fd2b9b850545
David Gómez <v-davgom@microsoft.com>
2019-07-04 09:59:17 +0200

    Create Test project to CoreTemplateStudio.Cli

---
+++ code/test/CoreTemplateStudio.Cli.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace CoreTemplateStudio.Cli.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
