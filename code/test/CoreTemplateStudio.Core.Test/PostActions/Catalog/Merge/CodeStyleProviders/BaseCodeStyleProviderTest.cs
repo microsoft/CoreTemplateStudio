4f79250819043de80636dc0261243e6574b56093
sibille <sibille@ilitia.com>
2019-10-28 07:55:23 +0100

    Fix stylecop issues

--- code/test/CoreTemplateStudio.Core.Test/PostActions/Catalog/Merge/CodeStyleProviders/BaseCodeStyleProviderTest.cs
+++ code/test/CoreTemplateStudio.Core.Test/PostActions/Catalog/Merge/CodeStyleProviders/BaseCodeStyleProviderTest.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Templates.Core.PostActions.Catalog.Merge.CodeStyleProviders;
-using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Templates.Core.PostActions.Catalog.Merge.CodeStyleProviders;
 using Xunit;
 
 namespace Microsoft.Templates.Core.Test.PostActions.Catalog.Merge.CodeStyleProviders
