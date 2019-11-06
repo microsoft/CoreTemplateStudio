1af8313a59eabf7094af6262589138324b7b7004
sibille <sibille@ilitia.com>
2019-11-06 11:56:49 +0100

    Set default version to 0.0.0.0

--- code/test/Utilities.Test/Properties/AssemblyInfo.cs
+++ code/test/Utilities.Test/Properties/AssemblyInfo.cs
@@ -21,4 +21,4 @@ using System.Runtime.InteropServices;
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
