eb4c0a1cafbc6b506b89abe2a55700aba3519930
sibille <sibille@ilitia.com>
2019-12-13 13:06:14 +0100

    Fix warnings

--- code/test/CoreTemplateStudio.Core.Test/Naming/Validators/FileNameValidatorTests.cs
+++ code/test/CoreTemplateStudio.Core.Test/Naming/Validators/FileNameValidatorTests.cs
@@ -2,10 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.Templates.Core.Naming;
 using System;
 using System.IO;
 using System.Linq;
+using Microsoft.Templates.Core.Naming;
 using Xunit;
 
 namespace Microsoft.Templates.Core.Test.Naming.Validators
--- code/test/CoreTemplateStudio.Core.Test/Naming/Validators/FolderNameValidatorTests.cs
+++ code/test/CoreTemplateStudio.Core.Test/Naming/Validators/FolderNameValidatorTests.cs
@@ -2,12 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.Templates.Core.Naming;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+using Microsoft.Templates.Core.Naming;
 using Xunit;
 
 namespace Microsoft.Templates.Core.Test.Naming.Validators
