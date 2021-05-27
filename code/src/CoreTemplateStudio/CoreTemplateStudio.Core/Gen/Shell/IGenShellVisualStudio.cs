6a9fa801daebf19c5ba3bca3895e1cd29366f91d
dgomezc <v-davgom@microsoft.com>
2021-05-27 12:34:06 +0200

    Create IGenShell interfaces

---
+++ code/src/CoreTemplateStudio/CoreTemplateStudio.Core/Gen/Shell/IGenShellVisualStudio.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.Templates.Core.Gen.Shell
+{
+    public interface IGenShellVisualStudio
+    {
+        string GetVsVersion(); // return "0.0.0.0";
+
+        string GetVsVersionAndInstance(); // return "0.0.0.0-i";
+
+        bool IsDebuggerEnabled();
+
+        bool IsBuildInProgress();
+
+        bool IsSdkInstalled(string version);
+    }
+}
